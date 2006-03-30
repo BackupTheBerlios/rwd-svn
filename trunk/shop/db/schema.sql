a6cf60b06210282486e1fc72518e352985e9f93e
filipcte <filipcte@a63dbd6e-fa0e-0410-bb28-bf70bca2a596>
2006-03-30 09:29:51 +0000

    database schema upload, 1st version

--- trunk/shop/db/schema.sql
+++ trunk/shop/db/schema.sql
@@ -1,2 +1,197 @@
--- $Id: medick.php 379 2006-03-18 17:36:03Z aurelian $
--- Database Schema for shop project
+-- phpMyAdmin SQL Dump
+-- version 2.8.0.2
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Mar 18, 2006 at 05:05 AM
+-- Server version: 4.1.16
+-- PHP Version: 5.1.2
+-- 
+-- Database: `medickart`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `authors`
+-- 
+
+CREATE TABLE `authors` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `first_name` varchar(100) NOT NULL default '',
+  `last_name` varchar(100) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `authors`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `bestsellers`
+-- 
+
+CREATE TABLE `bestsellers` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `product_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `bestsellers`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `categories`
+-- 
+
+CREATE TABLE `categories` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `parent_id` int(10) unsigned NOT NULL default '0',
+  `title` varchar(100) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  KEY `parent_id` (`parent_id`,`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `categories`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `order_details`
+-- 
+
+CREATE TABLE `order_details` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `order_id` int(10) unsigned NOT NULL default '0',
+  `product_title` varchar(200) NOT NULL default '',
+  `product_price` float NOT NULL default '0',
+  `ordered_quantity` smallint(6) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `order_details`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `orders`
+-- 
+
+CREATE TABLE `orders` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `first_name` varchar(100) NOT NULL default '',
+  `last_name` varchar(100) NOT NULL default '',
+  `company` varchar(100) NOT NULL default '',
+  `unique_reg_no` varchar(20) NOT NULL default '',
+  `address1` varchar(200) NOT NULL default '',
+  `address2` varchar(200) NOT NULL default '',
+  `phone` varchar(20) NOT NULL default '',
+  `fax` varchar(20) NOT NULL default '',
+  `email` varchar(60) NOT NULL default '',
+  `www` varchar(60) NOT NULL default '',
+  `created_on` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `orders`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `products`
+-- 
+
+CREATE TABLE `products` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `category_id` int(10) unsigned NOT NULL default '0',
+  `publisher_id` int(10) unsigned NOT NULL default '0',
+  `title` varchar(200) NOT NULL default '',
+  `technical_details` text NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL default '0',
+  `isbn` varchar(20) NOT NULL default '',
+  `available_qty` mediumint(9) NOT NULL default '0',
+  `image1` varchar(30) NOT NULL default '',
+  `image2` varchar(30) NOT NULL default '',
+  `image3` varchar(30) NOT NULL default '',
+  `created_on` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `updated_on` timestamp NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (`id`),
+  KEY `title` (`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `products`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `products_authors`
+-- 
+
+CREATE TABLE `products_authors` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `product_id` int(10) unsigned NOT NULL default '0',
+  `author_id` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `products_authors`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `publishers`
+-- 
+
+CREATE TABLE `publishers` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `title` varchar(200) NOT NULL default '',
+  `description` text NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `title` (`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `publishers`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `users`
+-- 
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `username` varchar(16) NOT NULL default '',
+  `password` varchar(32) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `users`
+-- 
+
