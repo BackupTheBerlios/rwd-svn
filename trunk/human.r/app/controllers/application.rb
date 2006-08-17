200e70dfebf4973f93d26667d48b7b358f2d764e
aurelian <aurelian@a63dbd6e-fa0e-0410-bb28-bf70bca2a596>
2006-08-17 11:51:03 +0000

    -- use only one layout for the application

--- trunk/human.r/app/controllers/application.rb
+++ trunk/human.r/app/controllers/application.rb
@@ -1,4 +1,9 @@
+#
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
+#
+# $Id$
+#
 class ApplicationController < ActionController::Base
+  layout "main"
 end
